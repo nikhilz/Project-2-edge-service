5a4b440d9d829a15bf77cc9fcb796198ba700caa
Nikhil S. Zurunge <nikhilzurunge8@gmail.com>
2020-02-27 16:22:43 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Project:2 edge-service
+ it will have fallback capabilities which prevent the client from receiving an HTTP error when the service is not available
---
+++ src/main/java/com/nikhil/itemCatelog/EdgeServiceApplication.java
@@ -0,0 +1,82 @@
+package com.nikhil.itemCatelog;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+import lombok.Data;
+
+@EnableFeignClients
+@EnableCircuitBreaker
+@EnableDiscoveryClient
+@EnableZuulProxy
+@SpringBootApplication
+public class EdgeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgeServiceApplication.class, args);
+	}
+
+}
+
+@Data
+class Item {
+    private String name;
+    
+    public String getName() {
+        return name; 
+}
+
+public void setName(String name) { 
+                 this.name = name;
+     }
+}
+
+@FeignClient("item-catalog-service")
+interface ItemClient {
+
+    @GetMapping("/items")
+    List<Item> readItems();
+}
+
+@RestController
+class GoodItemApiAdapterRestController {
+
+    private final ItemClient itemClient;
+
+    public GoodItemApiAdapterRestController(ItemClient itemClient) {
+        this.itemClient = itemClient;
+    }
+
+    @HystrixCommand(fallbackMethod = "fallback")
+    @GetMapping("/top-brands")
+    public Collection<Item> goodItems() {
+        return itemClient.readItems()
+                .stream()
+                .filter(this::isGreat)
+                .collect(Collectors.toList());
+    }
+
+    private boolean isGreat(Item item) {
+        return !item.getName().equals("Nike") &&
+                !item.getName().equals("Adidas") &&
+                !item.getName().equals("Reebok");
+    }
+    
+    public Collection<Item> fallback() {
+        return new ArrayList<>();
+    }
+}
